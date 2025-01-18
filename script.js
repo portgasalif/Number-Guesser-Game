3a6951f62025dd539b248e6b12dfb702204cc9ba
Alif Fachrel Anargya <aliffachrel@gmail.com>
2025-01-18 15:40:50 +0700

    add: game logic functions

--- script.js
+++ script.js
@@ -3,3 +3,27 @@ let computerScore = 0;
 let currentRoundNumber = 1;
 
 // Write your code below:
+const generateTarget = () => {
+    let randomInt = Math.floor(Math.random() * 10);
+
+    return randomInt;
+};
+
+const compareGuesses = (humanGuess, computerGuess, targetNumber) => {
+    const humanDiff = Math.abs(targetNumber - humanGuess);
+    const computerDiff = Math.abs(targetNumber - computerGuess);
+
+    if (humanDiff <= computerDiff) {
+        return true;
+    } else {
+        return false;
+    }
+};
+
+const updateScore = (winner) => {
+    if (winner === "human") {
+        humanScore += 1;
+    } else {
+        computerScore += 1;
+    }
+};
